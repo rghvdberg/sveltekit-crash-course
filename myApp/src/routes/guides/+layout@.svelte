5bc0c14cf117bd53644abebc57c25783422e0a0b
Rob van den Berg <rvdberg@bit.nl>
2023-05-05 17:22:32 +0200

    wip breakout layout

---
+++ myApp/src/routes/guides/+layout@.svelte
@@ -0,0 +1,21 @@
+<script>
+  // your script goes here
+  import Title from "$lib/Title.svelte"
+</script>
+
+<header>
+  <div class="links">
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/guides">Guides</a>
+  </div>
+</header>
+<slot/>
+<style>
+  .links {
+    margin-left: auto;
+  }
+  a {
+    margin-left: 10px
+  }
+</style>
